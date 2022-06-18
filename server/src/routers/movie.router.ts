30967fec6dccc468f1296c2ae978fdbe1a46b326
khaledCSE <khaledcse10@gmail.com>
2022-06-18 16:43:16 +0600

    Movie API done

--- server/src/routers/movie.router.ts
+++ server/src/routers/movie.router.ts
@@ -80,12 +80,69 @@ router.post('/', async (req: Request, res: Response) => {
 
 // Update
 router.patch('/', async (req: Request, res: Response) => {
-    res.json({ msg: 'Update Movie' });
+    try {
+        const ALLOWED_EXTENSIONS = /png|jpg|jpeg|webp/;
+        const { movieId, title, shortDescription, description, genre, rating, year } = req.body;
+        const to_update: any = {};
+
+        const movieFound = await Movie.findById(movieId);
+
+        if (!movieFound) {
+            return res.status(404).json({ msg: 'Movie not found' });
+        }
+
+        to_update.title = title;
+        to_update.shortDescription = shortDescription;
+        to_update.description = description;
+        to_update.genre = genre;
+        to_update.year = year;
+        to_update.rating = rating;
+
+        if (req.files) {
+            const ALLOWED_EXTENSIONS = /png|jpg|jpeg|webp/;
+            if (!req.files) {
+                return res.status(400).json({ message: 'No files received' });
+            }
+            const image: any = req.files.image;
+            // const { imageName } = req.body;
+            // const extName = imageName.split('.')[imageName.split('.').length - 1];
+            const extName = image.name.split('.')[image.name.split('.').length - 1];
+
+            if (!ALLOWED_EXTENSIONS.test(extName)) {
+                return res.status(400).json({ message: 'Only Image files are acceptable' });
+            }
+
+            // Save image
+            const url = `${path.resolve('server', 'uploads')}/${image.name}`;
+
+            await promisify(image.mv)(url);
+
+            const imageUrl = `${process.env.SERVER_ADDRESS}/uploads/${image.name}`;
+
+            to_update.image = imageUrl;
+        } else {
+            to_update.image = movieFound.image;
+        }
+
+        const updated = await Movie.findByIdAndUpdate(movieId, to_update);
+
+        res.json({ msg: 'Movie Updated' });
+    } catch (err: any) {
+        console.log(err);
+        res.status(500).json({ msg: 'Server error' });
+    }
 });
 
 // Delete
 router.delete('/:id', async (req: Request, res: Response) => {
-    res.json({ msg: 'Delete Movie' });
+    try {
+        const id = req.params;
+        const deleted = await Movie.findByIdAndRemove(id);
+        res.json({ msg: 'Movie deleted' });
+    } catch (err: any) {
+        console.log(err);
+        res.status(500).json({ msg: 'Server error' });
+    }
 });
 
 export default router;
